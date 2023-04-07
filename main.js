fb9943f0fa554cacfe00b88771a95bcdc77b6d67
MSMANI12 <selvamani12121998@gmail.com>
2023-04-07 12:08:05 +0530

    add js-using-page

---
+++ main.js
@@ -0,0 +1,19 @@
+let btns = document.querySelector(".btn");
+console.log(typeof btns);
+    btns.addEventListener('click', function (event) {
+        let targetElement = event.target; //
+        let parentDiv = targetElement.parentNode;
+        let paragraphDiv = parentDiv.getElementsByTagName('p');
+        let pElement = paragraphDiv[0];
+        pElement.classList.toggle('hide');
+    });
+	
+	 btns.addEventListener('mouseenter', function (event) {
+        let targetElement = event.target; //
+        let parentDiv = targetElement.parentNode;
+        let paragraphDiv = parentDiv.getElementsByTagName('p');
+        let pElement = paragraphDiv[0];
+        pElement.classList.toggle('hi');
+    });
+	
+	
